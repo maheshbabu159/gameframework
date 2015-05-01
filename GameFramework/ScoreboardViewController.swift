6c5e5b0db24ef03ad671ed512eec73cf8d880b49
maheshbabu159 <maheshbabu.somineni@gmail.com>
2015-05-01 17:52:20 +0530

    picker

--- GameFramework/ScoreboardViewController.swift
+++ GameFramework/ScoreboardViewController.swift
@@ -26,7 +26,7 @@ enum BBPhoto1: Int {
     case festen = 30
 }
 
-class ScoreboardViewController: UIViewController {
+class ScoreboardViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +53,23 @@ class ScoreboardViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK:Picker view delegate methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView,
+        numberOfRowsInComponent component: Int) -> Int {
+            
+            return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int,orComponent component: Int) -> String!{
+        
+        return "nssdf"
+    }
+
 
     /*
     // MARK: - Navigation
@@ -94,10 +111,77 @@ class ScoreboardViewController: UIViewController {
     @IBAction func saveButtonClick(sender:UIButton){
         
         
-        MatchEntityModel.saveMatchToDatabase(GlobalSingleton.sharedInstance.getRootMatch())
-        
+        //MatchEntityModel.saveMatchToDatabase(GlobalSingleton.sharedInstance.getRootMatch())
+        showPickerInActionSheet("asf")
         
     }
+    //MARK: Pickert view delegate methods
+    func showPickerInActionSheet(sentBy: String) {
+        
+        var title = ""
+        var message = "\n\n\n\n\n\n\n\n\n\n";
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.ActionSheet);
+        alert.modalInPopover = true;
+        
+        
+        //Create a frame (placeholder/wrapper) for the picker and then create the picker
+        var pickerFrame: CGRect = CGRectMake(17, 52, 270, 100); // CGRectMake(left), top, width, height) - left and top are like margins
+        var picker: UIPickerView = UIPickerView(frame: pickerFrame);
+        
+        
+        if(sentBy == ""){
+            picker.tag = 1;
+        } else if (sentBy == "user"){
+            picker.tag = 2;
+        } else {
+            picker.tag = 0;
+        }
+        
+        
+        //Add the picker to the alert controller
+        alert.view.addSubview(picker);
+        
+        //Create the toolbar view - the view witch will hold our 2 buttons
+        var toolFrame = CGRectMake(17, 5, 270, 45);
+        var toolView: UIView = UIView(frame: toolFrame);
+        
+        //add buttons to the view
+        var buttonCancelFrame: CGRect = CGRectMake(0, 7, 100, 30); //size & position of the button as placed on the toolView
+        
+        //Create the cancel button & set its title
+        var buttonCancel: UIButton = UIButton(frame: buttonCancelFrame);
+        buttonCancel.setTitle("Cancel", forState: UIControlState.Normal);
+        buttonCancel.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal);
+        toolView.addSubview(buttonCancel); //add it to the toolView
+        
+        //Add the target - target, function to call, the event witch will trigger the function call
+        buttonCancel.addTarget(self, action: "cancelSelection:", forControlEvents: UIControlEvents.TouchDown);
+        
+        
+        //add buttons to the view
+        var buttonOkFrame: CGRect = CGRectMake(170, 7, 100, 30); //size & position of the button as placed on the toolView
+        
+        //Create the Select button & set the title
+        var buttonOk: UIButton = UIButton(frame: buttonOkFrame);
+        buttonOk.setTitle("Select", forState: UIControlState.Normal);
+        buttonOk.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal);
+        toolView.addSubview(buttonOk); //add to the subview
+        
+        //Add the tartget. In my case I dynamicly set the target of the select button
+        if(sentBy == "profile"){
+            buttonOk.addTarget(self, action: "saveProfile:", forControlEvents: UIControlEvents.TouchDown);
+        } else if (sentBy == "user"){
+            buttonOk.addTarget(self, action: "saveUser:", forControlEvents: UIControlEvents.TouchDown);
+        }
+        
+        //add the toolbar to the alert controller
+        alert.view.addSubview(toolView);
+        
+        self.presentViewController(alert, animated: true, completion: nil);
+    }
+    
+
+    
     //MARK: Score update  methods
     func updateScores(enumValue : ButtonTags){
     
@@ -132,6 +216,25 @@ class ScoreboardViewController: UIViewController {
             
         }
 
+        
+        func saveProfile(sender: UIButton){
+            // Your code when select button is tapped
+            
+        }
+        
+        func saveUser(sender: UIButton){
+            // Your code when select button is tapped
+        }
+        
+        func cancelSelection(sender: UIButton){
+            println("Cancel");
+            self.dismissViewControllerAnimated(true, completion: nil);
+            // We dismiss the alert. Here you can add your additional code to execute when cancel is pressed
+        }
+        
+        
+        
+        
         
     }
    
